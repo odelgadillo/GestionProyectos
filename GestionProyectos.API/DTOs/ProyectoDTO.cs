714d6b0774972817f136e85d35b6845447f27eba
odelgadillo <odelgadillo@outlook.com>
2026-03-19 14:07:41 -0400

    Agregar validaciones de datos a Documentos

---
+++ GestionProyectos.API/DTOs/ProyectoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace GestionProyectos.API.DTOs
+{
+    public class ProyectoDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public int CantidadColaboradores { get; set; }
+        public int CantidadDocumentos { get; set; }
+    }
+}
--- GestionProyectos.API/Models/Documento.cs
+++ GestionProyectos.API/Models/Documento.cs
@@ -6,14 +6,16 @@ namespace GestionProyectos.API.Models
     {
         public int Id { get; set; }
 
-        [Required]
-        [StringLength(200)]
+        [Required(ErrorMessage = "El nombre del documento es obligatorio.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 100 caracteres.")]
         public string Nombre { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage ="El enlace o ruta es obligatorio.")]
+        [Url(ErrorMessage = "Por favor, ingresa una URL válida (http:// o https://.")]
         public string Enlace { get; set; } = string.Empty;
 
-        public string Tipo { get; set; } = "Enlace"; // Por defecto, se asume que es un enlace
+        [RegularExpression("^(PDF|Excel|Word|URL|Carpeta)$", ErrorMessage = "El tipo debe ser PDF, Excel, Word, URL o Carpeta.")]
+        public string Tipo { get; set; } = "URL";
 
         public int ProyectoId { get; set; }
         public Proyecto? Proyecto { get; set; }
