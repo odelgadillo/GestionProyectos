ad50a644b62b6bf4af6197122e67a3d74abb6ce4
Omar Horacio <odelgadillo@outlook.com>
2026-03-22 09:59:52 -0400

    corregir longitud de campo en Documentos y sincronizar migraciones entre estaciones

---
+++ GestionProyectos.API/Migrations/20260322135028_AjusteLongitudDocumentos.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionProyectos.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteLongitudDocumentos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Documentos",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Documentos",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
